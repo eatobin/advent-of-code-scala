cbf4c9028f11722f73eaaf40a9f828a2a6ac7fb0
Eric Tobin <eatobin@gmail.com>
2023-05-22 20:38:58 -0500

    clean up

--- src/test/scala/AdventOfCode2019/Day01Suite.scala
+++ src/test/scala/AdventOfCode2019/Day01Suite.scala
@@ -5,12 +5,6 @@ import org.scalatest.funsuite.AnyFunSuite
 class Day01Suite extends AnyFunSuite:
   val sample: Seq[Int] = Seq(100756)
 
-  val data: Seq[Int] = io.Source
-    .fromResource("AdventOfCode2019/Day01.txt")
-    .getLines()
-    .map(_.toInt)
-    .toSeq
-
   test("Part 1 should handle sample input correctly") {
     assert(Day01.part1(sample) == 33_583)
   }
