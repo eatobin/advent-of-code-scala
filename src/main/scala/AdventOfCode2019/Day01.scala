5d4c4c7195db1f88e5b41d382da6ce8c24efa6d5
Eric Tobin <eatobin@gmail.com>
2023-05-24 12:28:53 -0500

    gasPlus2!!!

--- src/main/scala/AdventOfCode2019/Day01.scala
+++ src/main/scala/AdventOfCode2019/Day01.scala
@@ -1,5 +1,7 @@
 package AdventOfCode2019
 
+import scala.annotation.tailrec
+
 object Day01:
   private def fuel(mass: Int): Int = (mass / 3) - 2
   private def tsiolkovsky(mass: Int): Int =
@@ -7,7 +9,18 @@ object Day01:
 
   def part1(input: Seq[Int]): Int = input.map(fuel).sum
   def part2(input: Seq[Int]): Int = input.map(tsiolkovsky).sum
-  def part3(input: Int): Int = input + 9
+
+  private def gasPlus2(mass: Int): Int =
+    @tailrec
+    def fuelAccumulator(m: Int, accum: Int): Int =
+      val newGas: Int = fuel(m)
+      if newGas > 0
+      then fuelAccumulator(m = newGas, accum = accum + newGas)
+      else accum
+
+    fuelAccumulator(m = mass, accum = 0)
+
+  private def part3(input: Seq[Int]): Int = input.map(gasPlus2).sum
 
   def main(args: Array[String]): Unit =
     val data = io.Source
@@ -17,3 +30,4 @@ object Day01:
       .toSeq
     println(part1(data)) // 3337766
     println(part2(data)) // 5003788
+    println(part3(data)) // 5003788
