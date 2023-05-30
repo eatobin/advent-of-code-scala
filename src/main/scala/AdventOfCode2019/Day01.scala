01e5badf9b0648680e940176805bbdbced3ea094
Eric Tobin <eatobin@gmail.com>
2023-05-30 16:09:55 -0500

    redo Day01.scala

--- src/main/scala/AdventOfCode2019/Day01.scala
+++ src/main/scala/AdventOfCode2019/Day01.scala
@@ -4,30 +4,23 @@ import scala.annotation.tailrec
 
 object Day01:
   private def fuel(mass: Int): Int = (mass / 3) - 2
-  private def tsiolkovsky(mass: Int): Int =
-    if mass < 7 then 0 else fuel(mass) + tsiolkovsky(fuel(mass))
-
-  def part1(input: Seq[Int]): Int = input.map(fuel).sum
-  def part2(input: Seq[Int]): Int = input.map(tsiolkovsky).sum
-
-  private def gasPlus2(mass: Int): Int =
+  private def fuelPlusFuel(mass: Int): Int =
     @tailrec
     def fuelAccumulator(m: Int, accum: Int): Int =
       val newGas: Int = fuel(m)
       if newGas > 0
       then fuelAccumulator(m = newGas, accum = accum + newGas)
       else accum
-
     fuelAccumulator(m = mass, accum = 0)
 
-  def part3(input: Seq[Int]): Int = input.map(gasPlus2).sum
+  def part1(input: Seq[Int]): Int = input.map(fuel).sum
+  def part2(input: Seq[Int]): Int = input.map(fuelPlusFuel).sum
 
   def main(args: Array[String]): Unit =
     val data = io.Source
-      .fromResource("AdventOfCode2019/Day01.txt")
+      .fromResource("Day01.txt")
       .getLines()
       .map(_.toInt)
       .toSeq
     println(part1(data)) // 3337766
     println(part2(data)) // 5003788
-    println(part3(data)) // 5003788
