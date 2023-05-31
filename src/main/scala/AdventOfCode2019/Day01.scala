d8ee31bbf65a35b2475cb5144d25f20681363aef
Eric Tobin <eatobin@gmail.com>
2023-05-31 17:55:18 -0500

    redo Day01.scala with extensions

--- src/main/scala/AdventOfCode2019/Day01.scala
+++ src/main/scala/AdventOfCode2019/Day01.scala
@@ -3,24 +3,37 @@ package AdventOfCode2019
 import scala.annotation.tailrec
 
 object Day01:
-  private def fuel(mass: Int): Int = (mass / 3) - 2
-  private def fuelPlusFuel(mass: Int): Int =
-    @tailrec
-    def fuelAccumulator(m: Int, accum: Int): Int =
-      val newGas: Int = fuel(m)
-      if newGas > 0
-      then fuelAccumulator(m = newGas, accum = accum + newGas)
-      else accum
-    fuelAccumulator(m = mass, accum = 0)
+  extension (int: Int)
+    private def fuel: Int =
+      (int / 3) - 2
 
-  def part1(input: Seq[Int]): Int = input.map(fuel).sum
-  def part2(input: Seq[Int]): Int = input.map(fuelPlusFuel).sum
+    private def fuelPlusFuel: Int =
+      @tailrec
+      def fuelAccumulator(mass: Int, accum: Int): Int =
+        val newGas: Int = mass.fuel
+        if newGas > 0
+        then fuelAccumulator(mass = newGas, accum = accum + newGas)
+        else accum
+      fuelAccumulator(mass = int, accum = 0)
+  end extension
+
+  private def part1(input: Seq[Int]): Int = input.map(fuel).sum
+  private def part2(input: Seq[Int]): Int = input.map(fuelPlusFuel).sum
 
   def main(args: Array[String]): Unit =
     val data = io.Source
-      .fromResource("Day01.txt")
+      .fromResource("AdventOfCode2019/Day01.txt")
       .getLines()
       .map(_.toInt)
       .toSeq
     println(part1(data)) // 3337766
     println(part2(data)) // 5003788
+
+    println(12.fuel) // 2
+    println(14.fuel) // 2
+    println(1969.fuel) // 654
+    println(100756.fuel) // 33583
+
+    println(14.fuelPlusFuel) // 2
+    println(1969.fuelPlusFuel) // 966
+    println(100756.fuelPlusFuel) // 50346
